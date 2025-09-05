76854e5f043c6cbd16c2b5bb4e909d6f95d0ce98
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-09-04 18:23:48 -0600

    Split the input string into an array of characters
    Added a Javascript function that splits an input string into characters

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    return inputString.split("");
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
