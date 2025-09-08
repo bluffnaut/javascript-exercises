439eb2d46c66253484a0a7ecda1d296bfcc41fa7
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-09-08 17:53:06 -0600

    Create a function to sum a range of numbers
    Function defines a range of numbers to be added up

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+// Error prevention:
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "Error";
+    if (min < 0 || max < 0) return "Error";
+    if (min > max) {
+        const temporaryMin = min;
+        max = temporaryMin;
+        min = max;
+    };
 
+    // Incremental loop:
+    let sum = 0;
+    for (i = min; i <= max; i++) {
+        sum += i;
+    };
+    return sum;
 };
+
+sumAll(1, 4); // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
